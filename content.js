ece1d6f7ce8ba1dd28030ba7fcad5b4e86871982
Eyrian Muet <eyrianmuet@gmail.com>
2024-09-12 13:20:23 +1100

    Rework de l'extension

--- content.js
+++ content.js
@@ -1,10 +1,4 @@
-// Sélectionnez toutes les images de la page
-const images = document.querySelectorAll("img");
-
-// URL de l'image locale (elle est exposée via web_accessible_resources)
-const imageUrl = chrome.runtime.getURL("icon.png");
-
-// Remplace chaque image par l'image locale
-images.forEach((img) => {
-    img.src = imageUrl;
-});
+var pageText = document.body.innerText;
+var $output = document.getElementById("output");
+$output.innerText = pageText;
+console.log(pageText);
