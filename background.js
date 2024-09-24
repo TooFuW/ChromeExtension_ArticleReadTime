7f49a2153445fb6f9e5be925b77f3a467d80e577
Eyrian Muet <eyrianmuet@gmail.com>
2024-09-25 08:31:49 +1100

    Reprise à 0

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'executeScript') {
-        chrome.tabs.executeScript({
-            code: 'document.body.innerText'
-        }, function(results) {
-            sendResponse(results[0]);
-        });
-    }
-});
